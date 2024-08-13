8da1ea2c9a3020736e2512445205e26869fa4934
biology1234 <hristianjordanov08@gmail.com>
2024-08-13 20:21:00 +0300

    Update arrays.js

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -24,14 +24,36 @@ console.log(subjects);
 
 //Remove element - last position - pop method
 
-subjects.pop();  //Remove the element of last position (Geography)
+subjects.pop();  //Remove Geography - last position
 console.log(subjects);
 
-console.log(subjects.pop()); //Shows which element removes     
+console.log(subjects.pop()); //Shows which element will remove     
 
+
+subjects.pop();             //Remove spanish - last position
+console.log(subjects);
 
 /*---------------------------------------------------------*/
 
 //Remove element - first position - shift method
+
+subjects.shift();        //Remove chemistry - first position 
+console.log(subjects);
+
+console.log(subjects.shift()); //Shows which element will remove 
+
+subjects.shift();  
+console.log(subjects);
+
+/*---------------------------------------------------------*/
+//Shows the index of element - indexOf method
+
+console.log(subjects.indexOf("English"));  //Shows that english is 0 index
+
+console.log(subjects.indexOf("Spanish")); //Spanish is not in the array => it has index -1
+
+/*---------------------------------------------------------*/
+//Shows whether an element exists in the array - includes method
+
 
 
